22b270168b43cd120175e1f63e4eb1d8a9b1bfa8
dipesh.tiwari <dipesh.tiwari@ranosys.com>
2018-04-26 14:49:21 +0530

    server.js updated

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 //Install express server
 const express = require('express');
 const app = express();
+var path = require('path');
+var morgan = require('morgan'); // log requests to the console (express4)
+var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var cors = require('cors');
+var fs = require('fs');
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
@@ -11,3 +17,4 @@ app.get('/hello', function(req, res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5050);
+console.log('node is running on port 5050');
