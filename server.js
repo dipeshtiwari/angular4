0a98e452b679c6508bfe27faabf1859765f2a942
dipesh.tiwari <dipesh.tiwari@ranosys.com>
2018-04-26 14:42:03 +0530

    server.js updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/hello', function(req, res) {
     res.send('hello');
@@ -11,5 +11,3 @@ app.get('/hello', function(req, res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5050);
-
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
