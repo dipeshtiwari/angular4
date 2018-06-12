867357b03593156d67b1979064dad01690f61f54
dipesh.tiwari <dipesh.tiwari@ranosys.com>
2018-06-12 12:43:56 +0530

    server.js updated

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 //Install express server
 const express = require('express');
 const app = express();
-var path = require('path');
-var fs = require('fs');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
+app.use(bodyParser.json()); // parse application/vnd.api+json as json
 
 app.get('/hello', function(req, res) {
     res.send('hello');
